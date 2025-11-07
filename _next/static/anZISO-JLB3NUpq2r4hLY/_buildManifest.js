449d463d8e02ff7d3de3f8e71978e8608ff1b1b1
acantoscurria <acantoscurria@users.noreply.github.com>
2025-11-07 23:25:17 +0000

    deploy: 0a5fe29ab22ace716134a60b09e5b90187810542

---
+++ _next/static/anZISO-JLB3NUpq2r4hLY/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/prevencion_digital//_next/:path+",
+        "destination": "/prevencion_digital/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
